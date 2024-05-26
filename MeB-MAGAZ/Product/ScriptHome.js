9a9bb304e4d6da556bed822c9ba8c34c89efa4dd
Теньгу <163237085+ManagoL@users.noreply.github.com>
2024-05-26 17:17:23 +0300

    Add files via upload

---
+++ MeB-MAGAZ/Product/ScriptHome.js
@@ -0,0 +1,49 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  }
+});
+
+
+
+
+let btnRigistr = document.getElementById('btnRigistr');
+let dialog = document.getElementById('dialog')
+var SVG = document.getElementsByClassName("close1")[0];
+document.body.classList.remove('modal-open');
+
+btnRigistr.addEventListener("click", ()=>{
+  dialog.showModal()
+  document.body.classList.add('modal-open');
+})
+
+SVG.addEventListener("click", () => {
+  dialog.close() 
+  document.body.classList.remove('modal-open');
+})
+
+
+document.addEventListener("keydown", (e) => {
+  if (e.code == "Escape") {
+    document.body.classList.remove('modal-open');
+  }
+});
