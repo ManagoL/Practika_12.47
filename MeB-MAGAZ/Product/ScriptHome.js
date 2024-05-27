e1a9af52cc28aa0ef0f264867e12c49237ae8c2a
Теньгу <163237085+ManagoL@users.noreply.github.com>
2024-05-27 14:12:02 +0300

    Add files via upload

--- MeB-MAGAZ/Product/ScriptHome.js
+++ MeB-MAGAZ/Product/ScriptHome.js
@@ -47,3 +47,15 @@ document.addEventListener("keydown", (e) => {
     document.body.classList.remove('modal-open');
   }
 });
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const destination = document.querySelector(this.getAttribute('href'));
+    if (destination) {
+      destination.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
